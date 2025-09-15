2656f396cef30bc77754c2fd40a2370866308f87
Sean Cheng <seanc2841@gmail.com>
2025-09-15 18:37:17 -0400

    repl remembers state

--- app/Main.hs
+++ app/Main.hs
@@ -9,6 +9,10 @@ import System.Environment
 import System.Exit
 import System.IO
 
+data Errs
+  = ScanOrParseErr
+  | RuntimeErr
+
 main :: IO ()
 main = do
   args <- getArgs
@@ -23,43 +27,62 @@ checkArgs _ = do
 
 -- | Creates Lox repl
 runPrompt :: IO () -- deal with environment persisting
-runPrompt = do
-  putStr "> "
-  hFlush stdout -- required to get the `>` to show up
-  input <- getLine
-  run input
-  runPrompt
+runPrompt = go defaultEnvironment
+  where
+    go env = do
+      putStr "> "
+      hFlush stdout -- required to get the `>` to show up
+      input <- getLine
+      errOrEnv <- runWithEnv env input
+      case errOrEnv of
+        Right newEnv -> go newEnv
+        Left _ -> go env
 
 -- | Runs a Lox file
 runFile :: String -> IO ()
 runFile filepath = do
   contents <- readFile filepath
-  run contents
+  errOrEnv <- runWithEnv defaultEnvironment contents
+  case errOrEnv of
+    Right _ -> return ()
+    Left ScanOrParseErr -> exitWith $ ExitFailure 65
+    Left RuntimeErr -> exitWith $ ExitFailure 70
 
-run :: String -> IO ()
-run contents = do
+runWithEnv :: Environment -> String -> IO (Either Errs Environment)
+runWithEnv env contents = do
   let scanResult = scanTokens contents
   case scanResult of
     Right tokens -> case parse tokens of
-      Right stmts -> runInterp defaultEnvironment stmts
+      Right stmts -> do
+        envOrErr <- go env stmts
+        case envOrErr of
+          Right newEnv -> return $ Right newEnv
+          Left _ -> return $ Left RuntimeErr
       Left errs -> do
         toStderr errs
-        exitWith $ ExitFailure 65
+        return $ Left ScanOrParseErr
     Left errs -> do
       toStderr errs
-      exitWith $ ExitFailure 65
+      return $ Left ScanOrParseErr
   where
     toStderr :: [String] -> IO ()
     toStderr [] = return ()
     toStderr (err : errs) = do
       hPutStrLn stderr err
       toStderr errs
-    runInterp :: Environment -> [Stmt] -> IO ()
-    runInterp _ [] = return ()
-    runInterp env (s : rest) = do
-      (newEnv, ok) <- interpret env s
-      case ok of
-        Right () -> runInterp newEnv rest
-        Left err -> do
-          hPutStrLn stderr err
-          exitWith $ ExitFailure 70
+    go :: Environment -> [Stmt] -> IO (Either String Environment)
+    go e (s : rest) = do
+      newEnvOrErr <- runInterp e s
+      case newEnvOrErr of
+        Left err -> return $ Left err
+        Right newEnv -> go newEnv rest
+    go e [] = return $ Right e
+
+runInterp :: Environment -> Stmt -> IO (Either String Environment)
+runInterp env s = do
+  (newEnv, ok) <- interpret env s
+  case ok of
+    Right () -> return $ Right newEnv
+    Left err -> do
+      hPutStrLn stderr err
+      return $ Left err
