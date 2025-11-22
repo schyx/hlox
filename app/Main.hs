9d44f84748967605322bf1d407127be9d7fdef3d
Sean Cheng <seanc2841@gmail.com>
2025-11-22 16:52:48 -0500

    add basic repl features

--- app/Main.hs
+++ app/Main.hs
@@ -1,5 +1,6 @@
 module Main (main) where
 
+import qualified Data.Map as Map
 import Error
 import Phases.Interpreter
 import Phases.Parse
@@ -10,19 +11,47 @@ import System.Environment
 import System.Exit
 import System.IO
 
--- TODO: add in repl behavior
-
 main :: IO ()
 main = do
   args <- getArgs
   checkArgs args
 
 checkArgs :: [String] -> IO ()
-checkArgs [] = undefined
+checkArgs [] = startShell
 checkArgs [file] = runFile file
 checkArgs _ = do
   putStrLn "Usage: hlox [script]"
   exitWith $ ExitFailure 64
+
+startShell :: IO ()
+startShell = go $ defaultInterpreter $ Locals Map.empty
+ where
+  go :: Interpreter -> IO ()
+  go interpreter = do
+    putStr "> "
+    hFlush stdout
+    input <- getLine
+    modifiedInterpreter <- runInput input interpreter
+    go modifiedInterpreter
+  runInput :: String -> Interpreter -> IO Interpreter
+  runInput input interpreter = do
+    let (scanErrs, tokens) = scanTokens input
+    if null scanErrs
+      then case parse tokens of
+        Right stmts -> do
+          case resolve stmts of
+            Left resolveErrs -> do
+              toStderr resolveErrs
+              return interpreter
+            Right _ -> do -- TODO: add in locals fix
+              statementOutput <- runStatements interpreter stmts
+              case statementOutput of
+                Left interpreterError -> toStderr [interpreterError] >> return interpreter
+                Right interpreter' -> return interpreter'
+        Left parseErrs -> do
+          toStderr parseErrs
+          return interpreter
+      else toStderr scanErrs >> return interpreter
 
 -- | Runs a Lox file
 runFile :: String -> IO ()
@@ -37,7 +66,7 @@ runFile filepath = do
             toStderr resolverErrs
             exitWith $ ExitFailure 65
           Right locals -> do
-            errOrEnv <- go (defaultInterpreter locals) stmts
+            errOrEnv <- runStatements (defaultInterpreter locals) stmts
             case errOrEnv of
               Right _ -> return ()
               Left _ -> do
@@ -52,14 +81,14 @@ runFile filepath = do
       Left parseErrs -> do
         toStderr $ scanErrs ++ parseErrs
         exitWith $ ExitFailure 65
- where
-  go :: Interpreter -> [SomeStmt] -> IO (Either Error Interpreter)
-  go e (s : rest) = do
-    newEnvOrErr <- runInterp e s
-    case newEnvOrErr of
-      Left err -> return $ Left err
-      Right newEnv -> go newEnv rest
-  go e [] = return $ Right e
+
+runStatements :: Interpreter -> [SomeStmt] -> IO (Either Error Interpreter)
+runStatements e (s : rest) = do
+  newEnvOrErr <- runInterp e s
+  case newEnvOrErr of
+    Left err -> return $ Left err
+    Right newEnv -> runStatements newEnv rest
+runStatements e [] = return $ Right e
 
 toStderr :: [Error] -> IO ()
 toStderr [] = return ()
